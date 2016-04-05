416004b0322c85ab5bb6f76bb62156a363a168be
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-05 09:33:18 -0700

    Limit JSON response to first five results

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
   model: function() {
     var url = 'http://congress.api.sunlightfoundation.com/bills?apikey=2a3b1ef2f9f5430fa83affc5c4322794';
     return Ember.$.getJSON(url).then(function(responseJSON){
-      return responseJSON.results;
+      return responseJSON.results.slice(0, 5);
     });
   },
   actions: {
