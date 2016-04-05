ab4924d595bd1cc16f8c010ec1af8880da405cd2
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-05 08:38:47 -0700

    Add model that gets JSON from API

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=2a3b1ef2f9f5430fa83affc5c4322794&zip=' + params.zip;
+    return Ember.$.getJSON(url).then(function(responseJSON){
+      return responseJSON.results;
+    });
+  }
 });
