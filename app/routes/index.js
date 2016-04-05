ccfaba943fe57aa170774a88fa8ea6a7e2b3ad97
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-05 09:07:53 -0700

    Add model to index page to get bills

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,15 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
+  model: function() {
+    var url = 'http://congress.api.sunlightfoundation.com/bills?apikey=2a3b1ef2f9f5430fa83affc5c4322794';
+    return Ember.$.getJSON(url).then(function(responseJSON){
+      return responseJSON.results;
+    });
+  },
   actions: {
     zipLookup(params) {
-      this.transitionTo('results', params.zip)
+      this.transitionTo('results', params.zip);
     }
   }
 });
