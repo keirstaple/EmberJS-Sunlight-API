9574255f9580f8bcffbe2b661fd0f160056a0b60
Keir Staple <keirstaple@comcast.net>
2016-05-05 16:57:41 -0700

    add code to environment.js and index.html to create live gh-pages link

--- config/environment.js
+++ config/environment.js
@@ -40,8 +40,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
-  }
+  ENV.baseURL = '/EmberJS-Sunlight-API/dist';
+  ENV.locationType = 'hash';
+}
 
   return ENV;
 };
